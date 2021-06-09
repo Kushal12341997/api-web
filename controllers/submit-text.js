bfff46e3d4f383e23aa634575c3c9ceebc3353a9
Kushal566 <kushal.471997@gmail.com>
2021-06-09 20:22:47 +0530

    api web v1 changes

--- controllers/submit-text.js
+++ controllers/submit-text.js
@@ -1,6 +1,21 @@
+const { default: axios } = require("axios");
+
 const handleTextSubmission = (req, res, next) => {
-  console.log("Text data :: ", req.body);
-  res.status(200).send("");
+  const requestBody = req.body;
+  console.log("Text data :: ", requestBody);
+  axios({
+    method: "POST",
+    url: "http://127.0.0.1:3002/model/summarize",
+    data: requestBody,
+  })
+    .then((response) => {
+      console.log("Response :: ", response);
+      res.status(200).send("");
+    })
+    .catch((err) => {
+      console.log("Error :: ", err);
+      res.status(502).send("");
+    });
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,14 +2,58 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+// const amqp = require("amqplib/callback_api");
 const textController = require("./controllers/submit-text");
 
 const app = express();
+
+// amqp.connect("amqp://localhost", (connectionErr, connection) => {
+//   if (connectionErr) {
+//     console.log("Unable to connect to rabbit mq :: ", err);
+//     return;
+//   }
+//   connection.createChannel((channelErr, channel) => {
+//     if (channelErr) {
+//       console.log("Unable to create connection channel :: ", err);
+//       return;
+//     }
+//     const queue = "hello";
+//     const msg = "Hello World";
+//     channel.assertQueue(queue, {
+//       durable: false,
+//     });
+
+//     setTimeout(() => {
+//       channel.sendToQueue(queue, Buffer.from(msg));
+//       console.log("Successfully sent message :: ", msg);
+//     }, 1000);
+
+//     // Adding consumption logic here
+//     setTimeout(() => {
+//       console.log("Waiting for messages on queue :: ", queue);
+//       channel.consume(
+//         queue,
+//         (msg) => {
+//           console.log("Received message :: ", msg.content.toString());
+//         },
+//         {
+//           noAck: true,
+//         }
+//       );
+//     }, 5000);
+//   });
+//   setTimeout(() => {
+//     connection.close();
+//     process.exit(0);
+//   }, 50000);
+// });
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static("www"));
 app.post("/api/submit-text", textController.handleTextSubmission);
+
 
 app.listen(3000, (err) => {
   if (err) {
